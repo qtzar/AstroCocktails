06d3d7319a109220b7a95b6749933d81010ab665
Declan Lynch <dlynch@qtzar.com>
2025-04-24 09:04:15 -0400

    feat: code updates

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,22 +3,23 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const cocktails = defineCollection({
-	loader: glob({ base: './docs', pattern: '**/*.{md,mdx}' }),
-	schema: z
-		.object({
-			title: z.string(),
-			description: z.string(),
-			spirits: z.array(z.string()),
-			liqueurs: z.array(z.string()).optional(),
-			ingredients: z.array(z.string()),
-			rating: z.number(),
-			glassware: z.string(),
-			styles: z.array(z.string()),
-			image: z.string().optional(),
-			source: z.string().optional(),
-			notes: z.string().optional(),
-		})
-		.passthrough()
+  loader: glob({ base: './docs', pattern: '**/*.{md,mdx}' }),
+  schema: z
+    .object({
+      title: z.string(),
+      description: z.string(),
+      spirits: z.array(z.string()),
+      liqueurs: z.array(z.string()).optional(),
+      ingredients: z.array(z.string()),
+      rating: z.number(),
+      glassware: z.string(),
+      method: z.string(),
+      styles: z.array(z.string()),
+      image: z.string().optional(),
+      source: z.string().optional(),
+      notes: z.string().optional(),
+    })
+    .passthrough(),
 });
 
 export const collections = { cocktails };
